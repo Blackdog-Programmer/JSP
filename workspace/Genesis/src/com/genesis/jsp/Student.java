f93c7ebbfa42eb4f04e33e59716f5653ab44272d
Liam, Song <blackdogcode@gmail.com>
2020-02-07 20:59:14 +0900

    JSTL Make HTML Table with Foreach

---
+++ workspace/Genesis/src/com/genesis/jsp/Student.java
@@ -0,0 +1,39 @@
+package com.genesis.jsp;
+
+public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private boolean isGoldCustomer;
+	
+	public Student(String firstName, String lastName, boolean isGoldCustomer) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.isGoldCustomer = isGoldCustomer;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public boolean isGoldCustomer() {
+		return isGoldCustomer;
+	}
+
+	public void setGoldCustomer(boolean isGoldCustomer) {
+		this.isGoldCustomer = isGoldCustomer;
+	}
+}
